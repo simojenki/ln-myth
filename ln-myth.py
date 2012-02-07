157f227ef861fc6a1f74ae96f542af359079bfdd
Simon <simonjenki@gmail.com>
2012-02-07 23:17:36 +0000

    getting closer'

--- ln-myth.py
+++ ln-myth.py
@@ -2,6 +2,7 @@
 
 from optparse import OptionParser
 import os
+import MySQLdb
 
 parser = OptionParser()
 parser.add_option("-u", "--user", action="store", dest="user")
@@ -10,8 +11,14 @@ parser.add_option("-d", "--destdir", action="store", dest="destdir")
 
 (options, args) = parser.parse_args()
 
-user = options.user 
-password = options.password
+db = MySQLdb.connect(host="localhost", port=3306, user=options.user, passwd=options.password, db="mythconverg")
+cursor = db.cursor()
+cursor.execute("select starttime, title, basename from recorded where basename like '%mpg'")
+recordings = [[recording[2], "%s %s.mpg" % (recording[0], recording[1])] for recording in cursor.fetchall()]
+db.close
+
+for filename in recordings :
+	print filename
 
 existingLns = set(os.listdir(options.destdir))
 for fname in existingLns:
