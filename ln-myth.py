52309c9b3890cf1090653425406a0fa045ebb293
Simon <simonjenki@gmail.com>
2012-02-08 07:42:17 +0000

    nearly done

--- ln-myth.py
+++ ln-myth.py
@@ -2,14 +2,18 @@
 
 from optparse import OptionParser
 import os
+import os.path
 import MySQLdb
 
 parser = OptionParser()
 parser.add_option("-u", "--user", action="store", dest="user")
 parser.add_option("-p", "--password", action="store", dest="password")
 parser.add_option("-d", "--destdir", action="store", dest="destdir")
+parser.add_option("-r", "--recordingsdir", action="store", dest="recordingsdir")
 
 (options, args) = parser.parse_args()
+
+os.chdir(options.destdir)
 
 db = MySQLdb.connect(host="localhost", port=3306, user=options.user, passwd=options.password, db="mythconverg")
 cursor = db.cursor()
@@ -17,11 +21,8 @@ cursor.execute("select starttime, title, basename from recorded where basename l
 recordings = [[recording[2], "%s %s.mpg" % (recording[0], recording[1])] for recording in cursor.fetchall()]
 db.close
 
-for filename in recordings :
-	print filename
+for toDelete in set(os.listdir(options.destdir)) - set([recording[1] for recording in recordings ]) :
+	os.remove(toDelete)
 
-existingLns = set(os.listdir(options.destdir))
-for fname in existingLns:
-    print fname
-
-#select starttime, title, basename from recorded;
+for recording in recordings :
+	if not(os.path.isfile(recording[1])) : os.link(options.recordingsdir + "/" + recording[0], recording[1]) 	
